e62b05a85484f089c5b96e11755c4d55fe66249b
yasuda <2001010382ur@cyber-u.ac.jp>
2024-05-29 22:09:05 +0900

    抽象クラスTaiyakiクラスの実装

--- TaiyakiAppModel/Taiyaki.cs
+++ TaiyakiAppModel/Taiyaki.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace TaiyakiAppModel
 {
-    internal class Taiyaki
+    public abstract class Taiyaki
     {
+        public FixedValue.Size Size
+            => _size;
+        public abstract string Name { get; }
+        public int GetPrice()
+        {
+            switch (_size)
+            {
+                case FixedValue.Size.小:
+                    return _price - 20;
+
+                case FixedValue.Size.大:
+                    return _price + 20;
+
+                default:
+                    return _price;
+            }
+        }
+
+        protected int _price;
+        private FixedValue.Size _size;
     }
 }
