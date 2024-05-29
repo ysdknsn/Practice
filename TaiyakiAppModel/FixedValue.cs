f941e45813919dc2d6700d486b44ef2dabd469bc
yasuda <2001010382ur@cyber-u.ac.jp>
2024-05-29 21:11:35 +0900

    規定値の設定

---
+++ TaiyakiAppModel/FixedValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiyakiAppModel
+{
+    public static class FixedValue
+    {
+        public enum Size
+        {
+            大,中,小
+        }
+
+        public const int CUSTARD_PRICE = 180;
+        public const int BEAN_PASTE_PRICE = 160;
+    }
+}
